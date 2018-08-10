f4631eefd1c965374a45cb434118ea8c79e9537f
Daniel <danielperezcascon@gmail.com>
2018-08-10 20:15:35 +0200

    Complete change to design Placed in class with init method Change in colour and message when outcome

--- GUI_rps.py
+++ GUI_rps.py
@@ -1,31 +1,44 @@
 from tkinter import *
 import random
 
-root = Tk()
-root.resizable(width=False, height=False)
 
-top = Frame(root, width=140, height=100)
-bot = Frame(root)
-top.pack()
-bot.pack()
+class UI:
 
-def play(val):
-    lose = {"s": "r", "p": "s", "r": "p"}
-    comp = random.choice(["r", "s", "p"])
+    def __init__(self, master):
+        top = Frame(master, width=140, height=100)
+        top.grid_propagate(0)
+        bot = Frame(master)
+        top.pack()
+        bot.pack()
 
-    #if comp == lose[val]:
+        label1 = Label(top, text="Lets Play!", )
+        label1.place(x=70, y=50, anchor="center")
 
+        rock = Button(bot, text="Rock", width=20, command=lambda: play("Rock")).grid(row=1)
+        paper = Button(bot, text="Paper", width=20, command=lambda: play("Paper")).grid(row=2)
+        scissors = Button(bot, text="Scissors", width=20, command=lambda: play("Scissors")).grid(row=3)
+
+        def play(val):
+            lose = {"Scissors": "Rock", "Paper": "Scissors", "Rock": "Paper"}
+            comp = random.choice(["Rock", "Scissors", "Paper"])
+
+            if comp == val:
+                label1.configure(text="You Drew!\nYou played:" + val + "\nComputer played: " + comp)
+                top.config()
+                label1.config()
+            elif comp == lose[val]:
+                label1.configure(text="You Won!\nYou played:" + val + "\nComputer played: " + comp)
+                top.config(bg="green")
+                label1.config(bg="green")
+            elif comp != lose[val]:
+                label1.configure(text="You Lost!\nYou played:" + val + "\nComputer played: " + comp)
+                top.config(bg="red")
+                label1.config(bg="red")
 
 # Creates a blank window no ability to be resized
-
-
-rock = Button(bot, text = "Rock", width = 20, command = lambda: play('r'))
-paper = Button(bot, text = "Paper", width = 20, command = lambda: play('p'))
-scissors = Button(bot, text = "Scissors", width = 20, command = lambda: play('s'))
-
-rock.grid(row = 1)
-paper.grid(row = 2)
-scissors.grid(row = 3)
+root = Tk()
+root.resizable(width=False, height=False)
+window = UI(root)
 
 # runs the GUI in an infinite loop untill you close the window
 root.mainloop()
