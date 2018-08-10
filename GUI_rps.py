be89617c70ac1cd8048adba175a9502fb2dcf383
Daniel <danielperezcascon@gmail.com>
2018-08-10 16:18:07 +0200

    Basic Changes to the GUI and checks of loss

--- GUI_rps.py
+++ GUI_rps.py
@@ -1,17 +1,27 @@
 from tkinter import *
+import random
 
-# Creates a blank window no ability to be resized
 root = Tk()
 root.resizable(width=False, height=False)
 
-canvas = Canvas(root, width=140, height=100)
+top = Frame(root, width=140, height=100)
 bot = Frame(root)
-canvas.pack()
+top.pack()
 bot.pack()
 
-rock = Button(bot, text = "Rock", width = 20)
-paper = Button(bot, text = "Paper", width = 20)
-scissors = Button(bot, text = "Scissors", width = 20)
+def play(val):
+    lose = {"s": "r", "p": "s", "r": "p"}
+    comp = random.choice(["r", "s", "p"])
+
+    #if comp == lose[val]:
+
+
+# Creates a blank window no ability to be resized
+
+
+rock = Button(bot, text = "Rock", width = 20, command = lambda: play('r'))
+paper = Button(bot, text = "Paper", width = 20, command = lambda: play('p'))
+scissors = Button(bot, text = "Scissors", width = 20, command = lambda: play('s'))
 
 rock.grid(row = 1)
 paper.grid(row = 2)
